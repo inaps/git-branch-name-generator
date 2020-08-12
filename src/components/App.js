6e33e6762b8bdc1a05eec42a32d1675b44eed076
Vitalii Prokhorov <vitalii_prokhorov@epam.com>
2020-08-12 11:38:24 +0300

    Version 1.1 wip

--- src/components/App.js
+++ src/components/App.js
@@ -3,73 +3,78 @@ import './App.scss';
 import Select from 'react-select'
 
 const STORE_KEYS = {
-  prefix: "prefix",
-  ticket: "ticket",
-  parsedTicket: "parsedTicket"
+  typeId: "typeId",
+  taskTitle: "taskTitle",
 }
-
 
 class App extends React.Component {
   state = {
-    parsedTicket: localStorage.getItem(STORE_KEYS.parsedTicket),
-    prefix: localStorage.getItem(STORE_KEYS.prefix),
+    taskTitle: localStorage.getItem(STORE_KEYS.taskTitle) || "",
+    typeId: localStorage.getItem(STORE_KEYS.typeId),
   }
   
   handleInput = (e) => {
-    const {value} = e.target;
-    localStorage.setItem(STORE_KEYS.ticket, value)
+    const {value: taskTitle} = e.target;
+    localStorage.setItem(STORE_KEYS.taskTitle, taskTitle)
     
-    const parsedTicket = value.replace(/ /g, "_")
-    this.setParsedTicket(parsedTicket)
-    localStorage.setItem(STORE_KEYS.parsedTicket, parsedTicket)
-  }
-  
-  handleSelect = (prefix) => {
-    let value = ""
-    if (prefix !== undefined) {
-      value = `${prefix}/`
-    }
-    localStorage.setItem(STORE_KEYS.prefix, value)
-    this.setPrefix(value)
-  }
-  
-  setParsedTicket = parsedTicket => {
     this.setState({
-      parsedTicket
+      taskTitle
     })
   }
   
-  
-  setPrefix = prefix => {
+  handleSelect = type => {
+    const {value: typeId} = type ? type : {};
+    localStorage.setItem(STORE_KEYS.typeId, typeId);
+    
     this.setState({
-      prefix
+      typeId
     })
+  }
+  
+  options = [
+    {value: "BUG", label: "bug"},
+    {value: "FEATURE", label: "feature"}
+  ]
+  
+  get type() {
+    return this.options.find(({value}) => value === this.state.typeId)
+  }
+  
+  get parsedTaskTitle() {
+    return this.state.taskTitle.trim().replace(/ /g, "_")
+  }
+  
+  get result() {
+    return `${this.type ? `${this.type.label}/` : ""}${this.parsedTaskTitle ? this.parsedTaskTitle : ""}`
+  }
+  
+  get defaultTaskTitle() {
+    return localStorage.getItem(STORE_KEYS.taskTitle)
   }
   
   render() {
-    const {prefix, parsedTicket} = this.state;
-    const result = `${prefix ? prefix : ""}${parsedTicket ? parsedTicket : ""}`
-    
     return (
       <div className="app">
-          {/*<Select*/}
-          {/*  isClearable*/}
-          {/*  placeholder="Type"*/}
-          {/*  className="typeSelector"*/}
-          {/*  defaultValue={prefix ? prefix.split("/")[0] : undefined}*/}
-          {/*  onChange={this.handleSelect}*/}
-          {/*  options={[*/}
-          {/*    {value: "BUG", label: "Bug"},*/}
-          {/*    {value: "FEATURE", label: "Feature"}*/}
-          {/*  ]}*/}
-          {/*/>*/}
-          {/*<input*/}
-          {/*  className="input"*/}
-          {/*  defaultValue={localStorage.getItem(STORE_KEYS.ticket)}*/}
-          {/*  onChange={this.handleInput}*/}
-          {/*  placeholder="Insert ticket here"*/}
-          {/*/>*/}
-          <textarea className="result"  placeholder="Result" />
+        <div className="contentWrapper">
+          <div className="form">
+            <Select
+              isClearable
+              placeholder="Type"
+              menuPosition="fixed"
+              className="selectType"
+              options={this.options}
+              defaultValue={this.type}
+              onChange={this.handleSelect}
+            />
+            <textarea
+              className="input"
+              onChange={this.handleInput}
+              placeholder="Task title"
+              defaultValue={this.defaultTaskTitle}
+            />
+          </div>
+        <textarea className="input" value={this.result} placeholder="Parsed result" />
+        </div>
       </div>
     );
   }
