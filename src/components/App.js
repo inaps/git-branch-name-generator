f1c71030502dceeffc4996ea33a45c792a27da1e
Vitalii Prokhorov <vitalii_prokhorov@epam.com>
2020-08-12 14:25:17 +0300

    Version 1.1

--- src/components/App.js
+++ src/components/App.js
@@ -52,28 +52,63 @@ class App extends React.Component {
     return localStorage.getItem(STORE_KEYS.taskTitle)
   }
   
+  customStyles = {
+    control: (styles, {isFocused}) => ({
+      ...styles,
+      background: "#585858",
+      boxShadow: "none",
+      border: isFocused ? "1px solid #a3a3a3": "1px solid #737373",
+      ':hover': {
+        ...styles[':hover'],
+        borderColor: "#a3a3a3",
+      },
+    }),
+    singleValue: styles => ({
+      ...styles,
+      color: "#eee",
+      fontFamily: "monospace",
+      fontSize: 14
+    }),
+    menuList: styles => ({
+      ...styles,
+      background: "#585858"
+    }),
+    option: (styles, {isSelected}) =>({
+      ...styles,
+      color: "#eee",
+      cursor: "pointer",
+      backgroundColor: isSelected ? "#474747" : "#585858",
+      ':hover': {
+        ...styles[':hover'],
+        backgroundColor: "#474747",
+      },
+    })
+  }
+  
   render() {
     return (
       <div className="app">
         <div className="contentWrapper">
+           <textarea
+             className="input"
+             onChange={this.handleInput}
+             placeholder="Task title"
+             defaultValue={this.defaultTaskTitle}
+           />
           <div className="form">
             <Select
               isClearable
               placeholder="Type"
+              isSearchable={false}
               menuPosition="fixed"
               className="selectType"
+              styles={this.customStyles}
               options={this.options}
               defaultValue={this.type}
               onChange={this.handleSelect}
             />
-            <textarea
-              className="input"
-              onChange={this.handleInput}
-              placeholder="Task title"
-              defaultValue={this.defaultTaskTitle}
-            />
+            <textarea className="input" value={this.result} placeholder="Parsed result" />
           </div>
-        <textarea className="input" value={this.result} placeholder="Parsed result" />
         </div>
       </div>
     );
