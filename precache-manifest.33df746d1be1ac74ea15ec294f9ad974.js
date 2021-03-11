5b251c39b7eaa111625734b6d0c9026ee9afa1e6
Vitalii Prokhorov <vitalii_prokhorov@epam.com>
2021-03-11 16:48:29 +0300

    build 1.6.2

---
+++ precache-manifest.33df746d1be1ac74ea15ec294f9ad974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a34d5a6aeb12d6b2d81c4e37bc4127",
+    "url": "/git-branch-name-generator/index.html"
+  },
+  {
+    "revision": "93d221fc1b2476d08a85",
+    "url": "/git-branch-name-generator/static/css/main.bb6a66dd.chunk.css"
+  },
+  {
+    "revision": "fddd166b296e16784ebd",
+    "url": "/git-branch-name-generator/static/js/2.67d958e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-branch-name-generator/static/js/2.67d958e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d221fc1b2476d08a85",
+    "url": "/git-branch-name-generator/static/js/main.4ec3e5f4.chunk.js"
+  },
+  {
+    "revision": "8640634f39c1bf8741a3",
+    "url": "/git-branch-name-generator/static/js/runtime-main.ea134831.js"
+  }
+]);
